585d12cf5bc267bf6f228b204f210ce12f10afc1
Alexander Sundvisson <nitrogaplays@gmail.com>
2022-05-06 13:01:53 +0200

    log-in, profile, animation

--- app.js
+++ app.js
@@ -4,14 +4,31 @@ function displaySettings() {
         document.querySelector(".settings").classList.toggle("pos");
     }
     document.querySelector(".settings").animate(settingsMove, animationDuration);
-    setTimeout(() => { document.querySelector(".settings").classList.toggle("pos"); }, 1020);
+    setTimeout(() => { document.querySelector(".settings").classList.toggle("pos"); }, 710);
+}
+
+function displayMenu() {
+    document.querySelector(".menu").classList.toggle("hidden");   
+    if (document.querySelector(".menu").classList.contains("pos")) {
+        document.querySelector(".menu").classList.toggle("pos");
+    }
+    document.querySelector(".menu").animate(settingsMove, animationDuration);
+    setTimeout(() => { document.querySelector(".menu").classList.toggle("pos"); }, 710);
+}
+
+function displayProfile() {
+    document.querySelector(".profile").classList.toggle("hidden");   
+    if (document.querySelector(".profile").classList.contains("pos")) {
+        document.querySelector(".profile").classList.toggle("pos");
+    }
+    document.querySelector(".profile").animate(settingsMove, animationDuration);
+    setTimeout(() => { document.querySelector(".profile").classList.toggle("pos"); }, 710);
 }
 
 const settingsMove = [
-    { transform: "translate(0, 70px)" }
+    { transform: "translate(0, 53px)" }
 ];
 
 const animationDuration = {
-    duration: 1000,
-    iterations: 1,
+    duration: 700,
 }
